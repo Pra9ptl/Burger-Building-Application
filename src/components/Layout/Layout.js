6dbc3a1502a36ecc9ea59f1f5d2eb4a8a09bcee7
Pra9ptl <patelpranav1313@gmail.com>
2020-04-08 12:38:01 -0400

    Enabling css modules and applying css in layout file using classes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Auxillary>
         <div>
             Toolbar, Sidebar, Backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxillary>
