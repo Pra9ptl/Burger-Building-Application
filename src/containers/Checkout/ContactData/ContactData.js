26c427d5ab81fed3a8f4e37545c008ab754bc38d
Pra9ptl <patelpranav1313@gmail.com>
2020-04-16 15:25:46 -0400

    passed all the ingredients from checkout component to contactData component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -12,16 +12,21 @@ class ContactData extends Component {
         }
     }
 
+    orderHandler = (event) => {
+        event.preventDefault();
+        console.log(this.props.ingredients);
+    }
+
     render() {
       return (
         <div className={classes.ContactData}>
           <h4>Enter your contact data</h4>
           <form>
-              <input type='text' name="name" placeholder="Enter your Name" />
-              <input type='text' name="mail" placeholder="Enter your Mail" />
-              <input type='text' name="street" placeholder="Enter your Street" />
-              <input type='text' name="postal" placeholder="Enter your Postal Code" />
-              <Button btnType="Success">Order</Button>
+              <input className={classes.Input} type='text' name="name" placeholder="Enter your Name" />
+              <input className={classes.Input} type='text' name="mail" placeholder="Enter your Mail" />
+              <input className={classes.Input} type='text' name="street" placeholder="Enter your Street" />
+              <input className={classes.Input} type='text' name="postal" placeholder="Enter your Postal Code" />
+              <Button btnType="Success" clicked={this.orderHandler} >Order</Button>
           </form>
         </div>
       )
