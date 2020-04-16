5f3ecb8a0c72875209fa925e7a1c09c0f0923d2e
Pra9ptl <patelpranav1313@gmail.com>
2020-04-16 16:26:08 -0400

    retriving data from server in orders and passing them to single order component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import Axiox from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
 
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        Axiox.get('/orders.json')
+            .then(response => {
+                const fetchOrders = [];
+                for (let key in response.data) {
+                    fetchOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchOrders });
+            }).catch(error => {
+                this.setState({ loading: false });
+
+            });
+    }
+
     render() {
-      return (
-        <div>
-          <Order />
-          <Order />
-        </div>
-      )
+        return (
+            <div>
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
+            </div>
+        )
     };
 }
 
-export default Orders;
+export default withErrorHandler(Orders, Axiox);
