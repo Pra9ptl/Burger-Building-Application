95c959eb4c303689e7ee0f8b203eb2a622cee0b3
Pra9ptl <patelpranav1313@gmail.com>
2020-04-25 16:41:31 -0400

    updated the folder structure in the store

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,7 +7,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actions';
 import { connect } from 'react-redux';
 
 class BurgerBuilder extends Component {
