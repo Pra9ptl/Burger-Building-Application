5a0d28c877bc180934cc87b2fa54128f14e46d90
Pra9ptl <patelpranav1313@gmail.com>
2020-04-26 15:45:53 -0400

    fetching all orders from server using redux state

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -42,3 +42,42 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     };
 };
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        Axios.get('/orders.json')
+            .then(response => {
+                const fetchOrders = [];
+                for (let key in response.data) {
+                    fetchOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchOrders))
+            }).catch(error => {
+                dispatch(fetchOrdersFail(error))
+            });
+    };
+};
