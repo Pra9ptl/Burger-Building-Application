53dfe4468355b2507f21723a8eab48e229e3d37e
Pra9ptl <patelpranav1313@gmail.com>
2020-04-27 12:26:12 -0400

    displaying loading state and error message if occurs

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
-
+import Sipnner from '../../components/UI/Spinner/Spinner';
 
 class Auth extends Component {
     state = {
@@ -107,7 +107,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementArray.map(formElement => (
+        let form = formElementArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementtype={formElement.config.elementType}
@@ -120,8 +120,21 @@ class Auth extends Component {
             />
         ));
 
+        if (this.props.loading) {
+            form = <Sipnner />
+        }
+
+        let errorMessage = null;
+
+        if (this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            );
+        }
+
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
@@ -134,10 +147,17 @@ class Auth extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
