41bdbf494e5d999656b0833caf5b4025d0222bbb
Pranav Patel <patelpranav1313@gmail.com>
2020-05-04 11:45:15 -0400

    custom httperror handler hook

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,41 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Auxillary from '../../hoc/Auxillary/Auxillary';
+import useHttpErrorHandler from '../../hooks/httpErrorHandler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return props => {
-        const [error, setError] = useState(null);
-
-        // can also use constructor for same
-        //as to run before component mounts we need to take out of the method in this situation
-        // componentWillMount() {
-        const reqInterceptor = axios.interceptors.request.use(req => {
-            setError(null);
-            return req;
-        })
-        const resInterceptor = axios.interceptors.response.use(res => res, err => {
-            setError(err);
-        });
-        // }
-
-        // clean up when it unmount
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.response.eject(resInterceptor);
-            };
-        }, [reqInterceptor, resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
-
+        const [error, clearErorr] = useHttpErrorHandler(axios);
         return (
             <Auxillary>
                 <Modal
                     show={error}
-                    modalClosed={errorConfirmedHandler}>
+                    modalClosed={clearErorr}>
                     {error && error.message}
                 </Modal>
                 <WrappedComponent {...props} />
